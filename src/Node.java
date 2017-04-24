e64a62722dc8e8e5bad699a52216ea12324a9d00
hhartwell <hhartwell@zagmail.gonzaga.edu>
2017-04-23 19:50:42 -0700

    fix fingers again

--- src/Node.java
+++ src/Node.java
@@ -111,16 +111,13 @@ public class Node {
     }
 
     public void fixFingers() {
-    	while (!fingers[0].online){
-			fingers[0] = fingers[0].findSucessor(fingers[0].identifier);
-    	}
-		for (int i=1; i < fingers.length; i++){
+		for (int i=0; i < fingers.length; i++){
     		if (!fingers[i].online){
     			/*
     			 * runs until an online node has been found, 
     			 * and there arent two entries of the same node in the finger table.
     			 */
-    			while (!fingers[i].online || fingers[i] != fingers[i-1]){
+    			while (!fingers[i].online && fingers[i] != fingers[i-1]){
     				fingers[i] = fingers[i].findSucessor(fingers[i].identifier);
     			}
     		}
