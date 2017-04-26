887960726c399bd61748a59a8c6e0e31a23b1af8
evanliferock <evanliferock@gmail.com>
2017-04-25 20:45:19 -0700

    Implemented removeNode

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
+import javax.swing.JOptionPane;
 
 public class Simulation{
    HashMap<Integer, Node> actives;
    HashMap<Integer, Node> deactives;
+   JOptionPane removeNotify;
    
    Simulation(){
       actives = new HashMap<Integer, Node>();
@@ -18,6 +20,19 @@ public class Simulation{
    }
    
    public void removeNode(){
-   
+      String input = JOptionPane.showInputDialog("What Node Should Be Removed?");
+      try{
+         int index = Integer.parseInt(input);
+         if(actives.get(index) == null)
+            JOptionPane.showMessageDialog(null, "Node " + index + " is not on Chord", 
+                                       "Alert", JOptionPane.ERROR_MESSAGE);
+         else{
+            actives.get(index).leave();
+            deactives.put(index, actives.remove(index));
+         }
+      }catch(NumberFormatException e){
+         JOptionPane.showMessageDialog(null, "That is not an integer", 
+                                       "Alert", JOptionPane.ERROR_MESSAGE);
+      }
    }
 }
