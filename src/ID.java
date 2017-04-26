a67e0a6df4ea2bac7c0cdb4cf7128e7fc61f606d
Ethan Mahintorabi <EthanMahintorabi@yahoo.com>
2017-04-25 20:31:59 -0700

    added a utiltiy function to ID to get its relative location in the key space

--- src/ID.java
+++ src/ID.java
@@ -1,10 +1,16 @@
 import java.lang.Comparable;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class ID implements Comparable<ID>{
     private BigInteger id;
+    private BigDecimal keySpaceSize = new BigDecimal(new BigInteger(1,new byte[]{
+            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF,
+            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF,
+            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF,
+            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF}));
 
     ID(byte[] ip){
         //does the SHA
@@ -15,7 +21,6 @@ public class ID implements Comparable<ID>{
             md.update(ip);
             byte[] hash = md.digest();
             this.id = new BigInteger(1, hash);
-
         }catch(NoSuchAlgorithmException e){
             e.printStackTrace();
         }
@@ -33,4 +38,9 @@ public class ID implements Comparable<ID>{
       return new ID(this.id.subtract(offset));
     }
 
+    public double getPercentage(){
+        BigDecimal currentKey = new BigDecimal(id);
+        BigDecimal percentage = currentKey.divide(keySpaceSize,30, BigDecimal.ROUND_HALF_DOWN);
+        return percentage.doubleValue();
+    }
 }
