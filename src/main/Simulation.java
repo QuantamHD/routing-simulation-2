f6dd51b3d91527b5bd553f75c29faefe1ae0e9ce
evanliferock <evanliferock@gmail.com>
2017-04-28 16:34:24 -0700

    Added a main to start error checking

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -2,11 +2,62 @@ package main;
 
 import java.util.HashMap;
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Arrays;
 
 public class Simulation{
    HashMap<Integer, Node> actives;
    HashMap<Integer, Node> deactives;
    JOptionPane removeNotify;
+   
+   public static void main(String[] args){
+      Simulation s = new Simulation();
+      Scanner in = new Scanner(System.in);
+      String str;
+      do{
+         // output
+         System.out.println("\nWhat would you like to do?");
+         System.out.println("'a':  Add a Node");
+         System.out.println("'a1': Add a previously removed Node");
+         System.out.println("'r':  Remove a Node");
+         System.out.println("'d':  Go to display options");
+         str = in.nextLine();
+         
+         // add
+         if(str.equals("a")){
+            s.addNode(true);
+            System.out.println("Node added");
+            
+         // add old
+         }else if(str.equals("a1")){
+            s.addNode(false);
+            System.out.println("Node added");
+            
+         // remove
+         }else if(str.equals("r")){
+            if(s.actives.size() < 1){
+               System.out.println("No nodes active");
+            }else{
+               Set<Integer> keys = s.actives.keySet();
+               System.out.println("Possible Options for removal: " +
+                     Arrays.toString(keys.toArray()));
+               
+               s.removeNode();
+            }
+         
+         // enter display options
+         }else if(str.equals("d")){
+            displayMode(s, in);
+         }
+      }while(!str.equals("q"));
+   }
+   
+   // For error checking a simulation
+   public static void displayMode(Simulation s, Scanner in){
+   
+   }
+   
    
    Simulation(){
       actives = new HashMap<Integer, Node>();
